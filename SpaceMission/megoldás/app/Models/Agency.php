e4ca1d31cbeb08a512fa2901030c8e0d01066545
Bubber007 <balint.lipot@gmail.com>
2025-06-02 18:56:44 +0200

    Add files via upload

---
+++ SpaceMission/megoldás/app/Models/Agency.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Agency extends Model
+{
+    protected $table = 'space_agencies';
+    protected $primaryKey = '_id';
+    protected $guarded = [];
+    public $timestamps = false;
+
+    protected $hidden = ['_id'];
+}
---
+++ SpaceMission/megoldás/app/Models/Commander.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Commander extends Model
+{
+    protected $table = 'mission_commanders';
+    protected $primaryKey = 'mission_id';
+    protected $guarded = [];
+    public $timestamps = false;
+
+    protected $hidden = ['mission_id'];
+
+    /**
+     * Get the mission that owns the Commander
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function mission(): BelongsTo
+    {
+        return $this->belongsTo(Mission::class, 'mission_id', '_id');
+    }
+}
